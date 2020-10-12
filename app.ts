a6aadbee9dc48926c1946c6ddd9af59fd36789b6
RomanSamohwal <rsamohwal@gmail.com>
2020-10-12 18:52:34 +0300

    add env for port

--- app.ts
+++ app.ts
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+//взять порт окружения и если не будет задан то взять 7767
+//хероку поставит по умолчанию свой порт
+let PORT = process.env.PORT || 3009;
 
 app.get('/', (req, res) => {
     res.send('Hello its websocket server');
@@ -15,6 +18,6 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-server.listen(3009, () => {
+server.listen(PORT, () => {
     console.log('listening on *:3000');
 });
